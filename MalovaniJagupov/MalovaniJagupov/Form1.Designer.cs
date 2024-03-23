5b61e9868885895a488d47d9a0e9123567f2023a
konstantiinn <koniag1905@gmail.com>
2024-03-23 22:16:56 +0100

    MalovaniJAgupov

---
+++ MalovaniJagupov/MalovaniJagupov/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace MalovaniJagupov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.guma = new System.Windows.Forms.Button();
+            this.shapeComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(260, 19);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(48, 46);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.colorPanel_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(188, 78);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pen",
+            "Pencil",
+            "Brush",
+            "Marker"});
+            this.comboBox1.Location = new System.Drawing.Point(188, 110);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CLICK TO SELECT COLOR - ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CHOOSE WIDTH - ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CHOOSE TYPE - ";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(880, 692);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(108, 54);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "CLEAR ALL";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // guma
+            // 
+            this.guma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.guma.Location = new System.Drawing.Point(13, 142);
+            this.guma.Name = "guma";
+            this.guma.Size = new System.Drawing.Size(44, 35);
+            this.guma.TabIndex = 6;
+            this.guma.Text = "guma";
+            this.guma.UseVisualStyleBackColor = false;
+            this.guma.Click += new System.EventHandler(this.guma_Click);
+            // 
+            // shapeComboBox
+            // 
+            this.shapeComboBox.FormattingEnabled = true;
+            this.shapeComboBox.Items.AddRange(new object[] {
+            "Rectangle",
+            "Oval",
+            "Triangle"});
+            this.shapeComboBox.Location = new System.Drawing.Point(454, 46);
+            this.shapeComboBox.Name = "shapeComboBox";
+            this.shapeComboBox.Size = new System.Drawing.Size(116, 21);
+            this.shapeComboBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(450, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "SELECT SHAPE";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(576, 46);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(251, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "TO DRAW SHAPE - CHECK";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(454, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(454, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(572, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "INPUT WIDTH";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(572, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "INPUT HEIGHT";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 758);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.shapeComboBox);
+            this.Controls.Add(this.guma);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button guma;
+        private System.Windows.Forms.ComboBox shapeComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MalovaniJagupov/MalovaniJagupov/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MalovaniJagupov
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint;
+        private Pen currentPen = new Pen(Color.Black, 2);
+        private string currentTool = "Pen";
+        private bool drawShapeMode = false;
+        
+
+        //eventy
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseClick += new MouseEventHandler(this.Form1_MouseClick);
+            this.Paint += Form1_Paint;
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+            this.MouseUp += Form1_MouseUp;
+            this.Load += Form1_Load;
+            checkBox1.CheckedChanged += new EventHandler(checkBox1_CheckedChanged);
+          
+        }
+
+        //malba
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawLine(pen, lastPoint, lastPoint);
+            }
+        }
+        //ukladani toho "vymalovaneho bodu" na platne
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            lastPoint = e.Location;
+        }
+
+        //currentTool - druh stetce
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = this.CreateGraphics())
+                {
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                    //Marker fixa pen pencil - druhy stetce
+                    switch (currentTool)
+                    {
+                        case "Pen":
+                            currentPen = new Pen(panel1.BackColor, (float)numericUpDown1.Value);
+                            break;
+                        case "Brush":
+                            currentPen = new Pen(panel1.BackColor, Math.Max((float)numericUpDown1.Value * 2, 1));
+                            currentPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                            currentPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                            break;
+                        case "Marker":
+                            
+                            Color markerColor = Color.FromArgb(128, panel1.BackColor); 
+                            currentPen = new Pen(markerColor, Math.Max((float)numericUpDown1.Value * 1.5f, 1));
+                            currentPen.StartCap = System.Drawing.Drawing2D.LineCap.Square;
+                            currentPen.EndCap = System.Drawing.Drawing2D.LineCap.Square;
+                            break;
+                        case "Pencil":
+                            currentPen = new Pen(panel1.BackColor, Math.Max((float)numericUpDown1.Value / 2, 1));
+                            currentPen.DashPattern = new float[] { 5, 2 }; 
+                            currentPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                            currentPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                            break;
+                        case "Eraser":
+                            //guma je bily pencil se stejnou tloustkou stetce
+                            currentPen = new Pen(Color.White, (float)numericUpDown1.Value);
+                            currentPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                            currentPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                            break;
+                    }
+
+
+                    g.DrawLine(currentPen, lastPoint, e.Location);
+                }
+                lastPoint = e.Location;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //musel jsem to rozkliknout protoze jinak designer.cs na me rve
+            
+ 
+            shapeComboBox.SelectedIndex = 0;
+
+        }
+        //CHATOPEN AI - panel na otevreni skoro vsech barev
+        private void colorPanel_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                panel1.BackColor = colorDialog.Color;
+            }
+        }
+        //vyber toolu neboli vyber kresleni objektu
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                currentTool = comboBox1.SelectedItem.ToString();
+                drawShapeMode = (currentTool == "Rectangle" || currentTool == "Oval" || currentTool == "Triangle");
+
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //musel jsem to rozkliknout protoze jinak designer.cs na me rve
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //smaz vsechno
+            using (Graphics g = this.CreateGraphics())
+            {
+               
+                g.Clear(this.BackColor);
+            }
+        }
+
+        private void guma_Click(object sender, EventArgs e)
+        {
+            currentTool = "Eraser";
+        }
+        //uzivatel ma na vyber 3 shapy - obdelnik, oval, trojuhelnik - automaticky se vybarvi do barvy pera
+        //maly bug!!! - nesmis hejbnout mysi v moment kresleni objektu, musis stat na miste, jinak se nakresli nejaka carka vybranym perem
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawShapeMode)
+            {
+                //kresli s tim co zadal user
+                int width, height;
+                if (int.TryParse(textBox1.Text, out width) && int.TryParse(textBox2.Text, out height))
+                {
+                    using (Graphics g = this.CreateGraphics())
+                    {
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                        using (Brush brush = new SolidBrush(panel1.BackColor))
+                        {
+                            switch (shapeComboBox.SelectedItem.ToString())
+                            {
+                                //obdelnik
+                                case "Rectangle":
+                                    g.FillRectangle(brush, e.X, e.Y, width, height);
+                                    break;
+                                //oval
+                                case "Oval":
+                                    g.FillEllipse(brush, e.X, e.Y, width, height);
+                                    break;
+                                case "Triangle":
+                                    //trojuhelnik
+                                    Point[] points = {
+                                new Point(e.X, e.Y),
+                                new Point(e.X - width / 2, e.Y + height),
+                                new Point(e.X + width / 2, e.Y + height)
+                            };
+                                    g.FillPolygon(brush, points);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    //kontrola vstupu useru
+                    MessageBox.Show("Invalid input. Please enter valid integer values for width and height.");
+                }
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            drawShapeMode = checkBox1.Checked;
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
